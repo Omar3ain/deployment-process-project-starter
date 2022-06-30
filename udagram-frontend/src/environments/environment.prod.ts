e07e7f7c6fb483447a6e186ec66fa7f9263cc5c7
Omar3ain <omar3ain@gmail.com>
2022-06-30 22:01:15 +0200

    updated envi

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:3000/api/v0",
+  apiHost: "udagram-api-dev.eba-3v8y82ct.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
